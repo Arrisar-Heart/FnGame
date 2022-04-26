023a05c7c02c7eac5da60a13ce22285df258a036
Arrisar Heart <Arrisar.Heart@gmail.com>
2022-04-26 10:12:15 +0300

    Initial Commit ~

---
+++ README.md
@@ -0,0 +1,1 @@
+# [UE4.27.2](https://github.com/Allar/ue5-style-guide)() { };
---
+++ Source/FnGame/Public/MainGM.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MainGM.generated.h"
+
+UCLASS()
+class FNGAME_API AMainGM : public AGameMode
+{
+  GENERATED_BODY()
+
+};
